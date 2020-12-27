f3ddd87f7e99a2ee6436c83f3bfe09fc14f123fb
Jmiguel14 <miguel.jurado@epn.edu.ec>
2020-12-26 22:39:23 -0500

    OK :)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -9,10 +9,13 @@ import { StatusBar } from '@ionic-native/status-bar/ngx';
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
 
-//import libraries 
+//Import libraries form firebase
 import {AngularFireModule} from '@angular/fire';
-import {AngularFireStorageModule} from '@angular/fire/storage';
+import {AngularFirestoreModule} from '@angular/fire/firestore';
+
+//Enviroment for firebase
 import { environment } from 'src/environments/environment';
+
 
 
 
@@ -25,7 +28,7 @@ import { environment } from 'src/environments/environment';
     AppRoutingModule,
 
     AngularFireModule.initializeApp(environment.firebaseConfig),
-    AngularFireStorageModule,
+    AngularFirestoreModule,
   ],
   providers: [
     StatusBar,
--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,77 @@
 import { Component } from '@angular/core';
+import {FirebaseService} from '../services/firebase.service';
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
+
+interface StudentData{
+  Name: string;
+  Age: number;
+  Address: string;
+}
 
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
+
 export class HomePage {
+  studentList=[];
+  studentData: StudentData;
+  studentForm: FormGroup;
 
-  constructor() {}
+  constructor(
+    private firebaseService: FirebaseService,
+    public fb: FormBuilder,
+  ) {
+    this.studentData={} as StudentData;
+  }
 
+  ngOnInit(){
+    this.studentForm=this.fb.group({
+      Name: ['', [Validators.required]],
+      Age: ['', [Validators.required]],
+      Address: ['', [Validators.required]],
+    })
+
+    this.firebaseService.read_student().subscribe(data => {
+      this.studentList=data.map(e=>{
+        return{
+          id:e.payload.doc.id,
+          isEdit: false,
+          Name: e.payload.doc.data()['Name'],
+          Age: e.payload.doc.data()['Age'],
+          Address: e.payload.doc.data()['Address'],
+        }
+      })
+      console.log(this.studentList);
+    });
+  }
+
+  CreateRecord() {
+    this.firebaseService.create_student(this.studentForm.value).then(res => {
+    console.log(res)  
+      this.studentForm.reset()
+    })
+    .catch(error => console.log(error))
+  }
+
+  RemoveRecord(rowID) {
+    this.firebaseService.delete_student(rowID);
+  }
+
+  EditRecord(record) {
+    record.isEdit = true;
+    record.EditName = record.Name;
+    record.EditAge = record.Age;
+    record.EditAddress = record.Address;
+  }
+
+  UpdateRecord(recordRow) {
+    let record = {};
+    record['Name'] = recordRow.EditName;
+    record['Age'] = recordRow.EditAge;
+    record['Address'] = recordRow.EditAddress;
+    this.firebaseService.update_student(recordRow.id, record);
+    recordRow.isEdit = false;
+  }
 }
